5adb8a29d0c8d6dc4f033ff019d919d9359348ef
LIA SALINAS PNEDA <lia11142013@gmail.com>
2015-09-07 13:06:59 -0500

    HOLA MUNDO 3

---
+++ HolaMundo3/src/holamundo3/HolaMundo3.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package holamundo3;
+
+/**
+ *
+ * @author juan
+ */
+public class HolaMundo3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Bienvenidos al control de versiones 3");
+    }
+    
+}
